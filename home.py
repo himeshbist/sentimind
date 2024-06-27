c5ee579056e28c97f0afea717460b2f3a85ed276
himeshbist <142923224+himeshbist@users.noreply.github.com>
2024-06-27 12:53:21 +0530

    Update home.py

--- home.py
+++ home.py
@@ -11,7 +11,7 @@ import logging
  
  
 def load_model():
-    model_path = r"C:\Users\Himesh\Downloads\new_senti\twitter_sentiment.pkl"
+    model_path = "model/twitter_sentiment.pkl"
     try:
         with open(model_path, 'rb') as f:
             model = pickle.load(f)
